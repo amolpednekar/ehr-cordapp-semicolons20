ae23cc4c06b9f87573cff7555663d7cae37d07e1
Amol Pednekar <amol0202@gmail.com>
2020-11-22 13:51:39 +0530

    CreateEhrFlow - Bootstrapping complete

--- workflows/src/main/kotlin/com/template/flows/CreateEhrFlow.kt
+++ workflows/src/main/kotlin/com/template/flows/CreateEhrFlow.kt
@@ -1,25 +1,57 @@
 package com.template.flows
 
 import co.paralleluniverse.fibers.Suspendable
+import com.template.contracts.EhrContract
+import com.template.states.EhrState
 import net.corda.core.flows.*
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
 
 
 @InitiatingFlow
 @StartableByRPC
-class CreateEhrFlowInitiator : FlowLogic<Unit>() {
-    override val progressTracker = ProgressTracker()
+class CreateEhrFlowInitiator : FlowLogic<SignedTransaction>() {
+
+    override val progressTracker: ProgressTracker = tracker()
+
+    companion object {
+        object CREATING : ProgressTracker.Step("Creating a new EHR")
+        object SIGNING : ProgressTracker.Step("Signing the EHR")
+        object VERIFYING : ProgressTracker.Step("Verifying the EHR")
+        object FINALISING : ProgressTracker.Step("Recording the EHR") {
+            override fun childProgressTracker() = FinalityFlow.tracker()
+        }
+
+        fun tracker() = ProgressTracker(CREATING, SIGNING, VERIFYING, FINALISING)
+    }
 
     @Suspendable
-    override fun call() {
-        // Initiator flow logic goes here.
+    override fun call(): SignedTransaction {
+        progressTracker.currentStep = CREATING
+
+        val ourIdentity = serviceHub.myInfo.legalIdentities.first()
+
+        val notary = serviceHub.networkMapCache.notaryIdentities.single()
+
+        val state = EhrState("Data", listOf(ourIdentity))
+        val utx = TransactionBuilder(notary = notary).addOutputState(state).addCommand(EhrContract.Commands.CreateEhrCommand(), listOf(this.ourIdentity.owningKey))
+
+        progressTracker.currentStep = SIGNING
+        val stx = serviceHub.signInitialTransaction(utx)
+
+        progressTracker.currentStep = VERIFYING
+        stx.verify(serviceHub)
+
+        progressTracker.currentStep = FINALISING
+        return subFlow(FinalityFlow(stx, listOf(), FINALISING.childProgressTracker()))
     }
 }
 
 @InitiatedBy(CreateEhrFlowInitiator::class)
-class CreateEhrFlowResponder(val counterpartySession: FlowSession) : FlowLogic<Unit>() {
+class CreateEhrFlowResponder(private val counterpartySession: FlowSession) : FlowLogic<SignedTransaction>() {
     @Suspendable
-    override fun call() {
-        // Responder flow logic goes here.
+    override fun call(): SignedTransaction {
+        return subFlow(ReceiveFinalityFlow(counterpartySession))
     }
 }
--- workflows/src/test/kotlin/com/template/FlowTests.kt
+++ workflows/src/test/kotlin/com/template/FlowTests.kt
@@ -1,23 +1,29 @@
 package com.template
 
+import com.template.flows.CreateEhrFlowInitiator
 import com.template.flows.Responder
+import com.template.states.EhrState
 import net.corda.testing.node.MockNetwork
 import net.corda.testing.node.MockNetworkParameters
 import net.corda.testing.node.TestCordapp
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.slf4j.LoggerFactory
 
 class FlowTests {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     private val network = MockNetwork(MockNetworkParameters(cordappsForAllNodes = listOf(
         TestCordapp.findCordapp("com.template.contracts"),
         TestCordapp.findCordapp("com.template.flows")
     )))
-    private val a = network.createNode()
-    private val b = network.createNode()
+    private val goaHospital = network.createNode()
+    private val puneHospital = network.createNode()
 
     init {
-        listOf(a, b).forEach {
+        listOf(goaHospital, puneHospital).forEach {
             it.registerInitiatedFlow(Responder::class.java)
         }
     }
@@ -29,7 +35,11 @@ class FlowTests {
     fun tearDown() = network.stopNodes()
 
     @Test
-    fun `dummy test`() {
-
+    fun `create a EHR`() {
+        val flow = CreateEhrFlowInitiator()
+        goaHospital.startFlow(flow).toCompletableFuture()
+        network.runNetwork()
+        val result = goaHospital.services.vaultService.queryBy(contractStateType = EhrState::class.java).states
+        logger.info("Create EHR: $result")
     }
 }
